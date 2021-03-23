4a8b1a44e34ffd024a6f5c0696019d0ed943df52
Justin Chen <justinchen044@gmail.com>
2021-03-22 22:25:28 -0400

    finished compass

--- core/src/com/mygdx/game/Gyro.java
+++ core/src/com/mygdx/game/Gyro.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Gyro extends ApplicationAdapter {
@@ -14,14 +15,21 @@ public class Gyro extends ApplicationAdapter {
 	BitmapFont font;
 	String message;
 	float highestY;
-
-
+	Texture compass;
+	Sprite compassSprite;
+	float azimuth;
+	static final float ALPHA = 0.1f;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		font = new BitmapFont();
 		font.setColor(Color.RED);
-		font.getData().setScale(3);
+		font.getData().setScale(3, 3);
+		compass = new Texture(Gdx.files.internal("compass.png"));
+		compassSprite = new Sprite(compass);
+		compassSprite.setOriginCenter();
+		compassSprite.setPosition(Gdx.graphics.getWidth()/2 - compassSprite.getWidth()/2, Gdx.graphics.getHeight()/2 - compassSprite.getWidth()/2);
+		azimuth = 0f;
 	}
 
 	@Override
@@ -45,9 +53,6 @@ public class Gyro extends ApplicationAdapter {
 			case Portrait:
 				message += " portrait. \n";
 				break;
-			default:
-				message += " complete crap!\n";
-				break;
 		}
 		message += "Device Resolution: " + Integer.toString(w) + "," + Integer.toString(h) + "\n";
 		message += "Y axis accel: " + Float.toString(accelY) + " \n";
@@ -57,20 +62,35 @@ public class Gyro extends ApplicationAdapter {
 				Gdx.input.vibrate(100);
 			}
 		}
+		azimuth = azimuth + ALPHA * (Gdx.input.getAzimuth() - azimuth);
 		if(Gdx.input.isPeripheralAvailable(Input.Peripheral.Compass)){
-			message += "Azmuth:" + Float.toString(Gdx.input.getAzimuth()) + "\n";
+			message += "Azmuth:" + Float.toString(Gdx.input.getAzimuth()) + " " + azimuth + "\n";
 			message += "Pitch:" + Float.toString(Gdx.input.getPitch()) + "\n";
 			message += "Roll:" + Float.toString(Gdx.input.getRoll()) + "\n";
+			message += "Sprite rotation:" + Float.toString(compassSprite.getRotation());
 		}
 		else{
 			message += "No compass available\n";
 		}
 
+
+
 		font.draw(batch, message, 0, h);
+
+		compassSprite.setRotation(azimuth);
+		compassSprite.draw(batch);
 		batch.end();
 
 	}
-	
+
+	@Override
+	public void resize(int width, int height){
+		batch.dispose();
+		batch = new SpriteBatch();
+		compassSprite.setPosition(Gdx.graphics.getWidth()/2 - compassSprite.getWidth()/2, Gdx.graphics.getHeight()/2 - compassSprite.getWidth()/2);
+		String resolution = Integer.toString(width) + "," + Integer.toString(height);
+		Gdx.app.log("MJF", "Resolution changed " + resolution);
+	}
 	@Override
 	public void dispose () {
 		batch.dispose();
